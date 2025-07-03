22147b726806e0c8f7c0fc2d69d0c4e5905ab5b5
DavidCárcelesBorda <DavidCrcelesBorda@DESKTOP-G3SJU9F>
2025-07-03 13:31:13 +0200

    primer commit

--- WinFormsApp2/Program.cs
+++ WinFormsApp2/Program.cs
@@ -13,9 +13,7 @@ namespace WinFormsApp2
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
             Console.WriteLine("hola");
-            Console.WriteLine("hola2");
-            Console.WriteLine("hola3");
-            
+            Console.WriteLine("hola");
         }
     }
 }
