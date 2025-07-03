fe0995c4a142196f1aeb87f92a45ee867a54c682
DavidCárcelesBorda <DavidCrcelesBorda@DESKTOP-G3SJU9F>
2025-07-03 16:32:48 +0200

    w

--- WinFormsApp2/Program.cs
+++ WinFormsApp2/Program.cs
@@ -12,8 +12,10 @@ namespace WinFormsApp2
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            
->>>>>>> primer commit
+            Console.WriteLine("hola");
+<<<<<<< HEAD
+           
+
         }
         
     }
