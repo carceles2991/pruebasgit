00e958579fff7c6a33412904821519e44a695fc8
carceles2991 <100295113+carceles2991@users.noreply.github.com>
2025-07-04 09:35:55 +0200

    Update Program.cs

--- WinFormsApp2/Program.cs
+++ WinFormsApp2/Program.cs
@@ -13,7 +13,8 @@ namespace WinFormsApp2
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
             Console.WriteLine("esto es la ram2");
-            Console.WriteLine("esto es la ram3");
+            Console.WriteLine("esto es la ram2");
+            Console.WriteLine("esto es la ram2");
            
         }
     }
